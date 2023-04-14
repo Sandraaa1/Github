90d8c091a0306d654a2e40e4ad9422b0b4c239a3
aemlocal <aemlocal@ED49005994P048.educa.jcyl.es>
2023-04-14 13:24:58 +0200

    rt5

--- src/Prueba.java
+++ src/Prueba.java
@@ -4,6 +4,8 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 		//vbbd
+		System.out.println("HOLA");
+		System.out.println("HOLA2");
 	}
 
 }
